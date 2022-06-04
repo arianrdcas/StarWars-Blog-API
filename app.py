54f6d0a3e958f15daa10372c172e36987b7e59ad
Arian <arianrdcas@gmail.com>
2022-06-03 20:16:46 -0400

    comenzando definir rutas

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request, jsonify
+from flask_migrate import Migrate
+from flask_cors import CORS
+from models import db, User
 
 app = Flask(__name__)
+
+app.url_map.strict_slashes=False
+app.config['DEBUG']=True
+app.config['ENV']='development'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///database.db'
+
+db.init_app(app)
+Migrate(app,db) #db init, db migrate, db upgrade
+CORS(app)
 
 
 @app.route("/", methods=['GET'])
@@ -9,4 +22,4 @@ def main():
 
 
 if __name__=='__main__':
-    app.run(debug=True) 
+    app.run() 
--- models.py
+++ models.py
@@ -1,1 +1,32 @@
- 
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'user'
+    iduser = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    nameuser = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    emailuser = db.Column(db.String(250), nullable=False, unique=True)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.first_name,
+            "last_name": self.last_name,
+            "nameuser": self.nameuser,
+            "emailuser": self.emailuser
+        }
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
