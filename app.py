c938273b801b53b13f517e859b895fab72c105bd
Arian <arianrdcas@gmail.com>
2022-06-07 00:23:53 -0400

    CRUS Usuario ok

--- app.py
+++ app.py
@@ -27,7 +27,53 @@ def get_users():
 
     return jsonify(users), 200
 
+@app.route('/api/users', methods=['POST'])
+def post_users():
 
+    name = request.json.get('name')
+    last_name = request.json.get('last_name')
+    nameuser = request.json.get('nameuser')
+    password = request.json.get('password')
+    emailuser = request.json.get('emailuser')
+
+    user = User()
+    user.name = name
+    user.last_name = last_name
+    user.nameuser = nameuser
+    user.password = password
+    user.emailuser = emailuser
+    user.save()
+
+    return jsonify(user.serialize()), 201
+
+
+@app.route('/api/users/<int:id>', methods=['PUT'])
+def put_users(id):
+
+    name = request.json.get('name')
+    last_name = request.json.get('last_name')
+    nameuser = request.json.get('nameuser')
+    password = request.json.get('password')
+    emailuser = request.json.get('emailuser')
+
+    user = User.query.get(id)
+    user.name = name
+    user.last_name = last_name
+    user.nameuser = nameuser
+    user.password = password
+    user.emailuser = emailuser
+    user.update()
+    
+    return jsonify(user.serialize()), 200
+
+
+@app.route('/api/users/<int:id>', methods=['DELETE'])
+def delete_users(id):
+
+    user = User.query.get(id)
+    user.delete()
+    
+    return jsonify({ "status": True, "msg": "Usuario elminado"}), 200
 
 if __name__=='__main__':
     app.run()  
